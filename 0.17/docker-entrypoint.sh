d0421046c1cf860da8329e0e3a517b034e28e8d2
Rui Marinho <ruipmarinho@gmail.com>
2019-09-15 01:52:43 +0100

    Only chown and chmod datadir if container user is not set

--- 0.17/docker-entrypoint.sh
+++ 0.17/docker-entrypoint.sh
@@ -9,18 +9,16 @@ fi
 
 if [ $(echo "$1" | cut -c1) = "-" ] || [ "$1" = "litecoind" ]; then
   mkdir -p "$LITECOIN_DATA"
-  chmod 700 "$LITECOIN_DATA"
-  chown -R litecoin "$LITECOIN_DATA"
+  chmod 770 "$LITECOIN_DATA" || echo "Could notchmod $LITECOIN_DATA (may not have appropriate permissions)"
+  chown -R litecoin "$LITECOIN_DATA" || echo "Could notchown $LITECOIN_DATA (may not have appropriate permissions)"
 
   echo "$0: setting data directory to $LITECOIN_DATA"
 
   set -- "$@" -datadir="$LITECOIN_DATA"
 fi
 
-if [ "$1" = "litecoind" ] || [ "$1" = "litecoin-cli" ] || [ "$1" = "litecoin-tx" ]; then
-  echo
-  exec gosu litecoin "$@"
+if [ "$(id -u)" = "0" ] && ([ "$1" = "litecoind" ] || [ "$1" = "litecoin-cli" ] || [ "$1" = "litecoin-tx" ]); then
+  set -- gosu litecoin "$@"
 fi
 
-echo
 exec "$@"
